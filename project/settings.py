fda5858074a3cb471226a0613ad8a339c3962012
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-09-24 15:55:01 +0200

    add send gmail

--- project/settings.py
+++ project/settings.py
@@ -136,6 +136,18 @@ MEDIA_ROOT= BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 AUTHENTICATION_BACKENDS=['accounts.backend.Login_UserName_Email']
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'mohamedelazab2017@gmail.com'
+EMAIL_HOST_PASSWORD = 'txwslxkjzcgdtqzd'
+
+EMAIL_USE_SSL = False
